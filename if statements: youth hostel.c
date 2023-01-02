ec96123fbf3dc8d88d27b07b0d6f5496b5926081
Bengie Villesco <121591269+p3uj@users.noreply.github.com>
2023-01-02 14:19:52 +0800

    Create if statements: youth hostel.c

---
+++ if statements: youth hostel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int arrival = 0;
+
+    scanf("%d", &arrival);
+    if (arrival < 10)
+        printf("%d", 10+arrival*5);
+    else
+        printf("53");
+}
