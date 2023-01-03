d83fe1d2a21c0424c91cf77e789305d95996bbd8
Bengie Villesco <121591269+p3uj@users.noreply.github.com>
2023-01-03 15:27:24 +0800

    Create repeat a word read from the user input.c

---
+++ repeat a word read from the user input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char word[101];
+    int num, i;
+
+    scanf("%d %s", &num, word);
+    //scanf("%s", word);
+    for (i = 0; i < num; i++)
+        printf("%s\n", word);
+    return 0; 
+}
